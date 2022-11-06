79ac8f3d28cd651916e9cb9ee6a79fc28834ffa4
Abu <abughalib64@gmail.com>
2022-11-07 00:57:32 +0530

    caching & code cleanup

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,63 +1,76 @@
 import * as esbuild from "esbuild-wasm";
+import localforage from "localforage";
 
 const UNPKG_PATH = "https://unpkg.com";
 
-export const unpkgPathPlugin = () => {
+const fileCache = localforage.createInstance({
+  name: "filecache",
+});
+
+export const unpkgPathPlugin = (input: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
+      build.onResolve({ filter: /^(index\.js)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      build.onResolve({ filter: /^\.+\// }, (args: esbuild.OnResolveArgs) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, UNPKG_PATH + args.resolveDir + "/").href,
+        };
+      });
+
+      build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
         console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              UNPKG_PATH + args.resolveDir + "/"
-            ).href,
-          };
-        }
-
         return {
           namespace: "a",
           path: `${UNPKG_PATH}/${args.path}`,
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
+      build.onLoad(
+        { filter: /.*/ },
+        async (args: esbuild.OnLoadArgs): Promise<esbuild.OnLoadResult> => {
+          console.log("onLoad", args);
 
-        if (args.path === "index.js") {
-          return {
+          if (args.path === "index.js") {
+            return {
+              loader: "jsx",
+              contents: input,
+            };
+          }
+
+          // Check if the file is in the cache
+          const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(
+            args.path
+          );
+
+          // If it is, return it immediately
+          if (cacheResult) {
+            return cacheResult;
+          }
+
+          // If not, fetch it
+          let resp = await fetch(args.path, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/javascript",
+            },
+          });
+
+          const result: esbuild.OnLoadResult = {
             loader: "jsx",
-            contents: `
-              import React from 'react';
-              import ReactDOM from 'react-dom';
-
-              console.log(React, ReactDOM);
-            `,
+            contents: await resp.text(),
+            resolveDir: new URL("./", resp.url).pathname,
           };
+
+          fileCache.setItem(args.path, result);
+
+          return result;
         }
-
-        let resp = await fetch(args.path, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/javascript",
-          },
-        });
-
-        let contents = await resp.text();
-
-        return {
-          loader: "jsx",
-          contents: contents,
-          resolveDir: new URL("./", resp.url).pathname,
-        };
-      });
+      );
     },
   };
 };
