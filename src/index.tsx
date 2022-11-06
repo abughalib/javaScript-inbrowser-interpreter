932d09b172b9b8485f4267bac94c3c49f17c9e34
Abu <abughalib64@gmail.com>
2022-11-06 20:14:38 +0530

    esbuild config

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import * as esbuild from "esbuild-wasm";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: "./esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    let result = await esbuild.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    console.log(result);
+
+    setCode(result.code);
+  };
+
+  return (
+    <div>
+      <textarea onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button type="button" onClick={onClick}>
+          Submit
+        </button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
+root.render(<App />);
