12b18f3adc227e1ba2d565a10a4db7948763a2af
Abu <abughalib64@gmail.com>
2022-12-14 16:13:34 +0530

    preview cleanup

---
+++ src/components/preview.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from "react";
+
+interface PreviewProps {
+  code: string;
+}
+
+let htmlContent = `
+    <html>
+        <head></head>
+      <body>
+        <div id="root"><i>Your output would be here...</i></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              let message = "<div style='font-size: 20px;'>Runtime Error</div>"+err;
+              console.error(message);
+              throw err;
+            }
+          })
+        </script>
+        <script>
+          console = {
+            log(message) {
+              document.getElementById('root').innerHTML = message;
+            },
+            info(message) {
+              document.getElementById('root').innerHTML = message;
+              document.getElementById('root').style.color = 'orange';
+            },
+            error(message) {
+              document.getElementById('root').innerHTML = message;
+              document.getElementById('root').style.color = 'red';
+            }
+          }
+        </script>
+      </body>
+    </html>
+  `;
+
+const Preview: React.FC<PreviewProps> = ({ code }) => {
+  const iframe = useRef<any>();
+
+  useEffect(() => {
+    iframe.current.srcdoc = htmlContent;
+    iframe.current.contentWindow.postMessage(code, "*");
+  }, [code]);
+
+  return (
+    <iframe
+      ref={iframe}
+      sandbox="allow-scripts"
+      title="result"
+      srcDoc={htmlContent}
+    />
+  );
+};
+
+export default Preview;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import Preview from "./components/preview";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const iframe = useRef<any>();
+  const [code, setCode] = useState("");
 
   const startService = async () => {
     await esbuild.initialize({
@@ -21,44 +22,7 @@ const App = () => {
     startService();
   }, []);
 
-  let htmlContent = `
-    <html>
-        <head></head>
-      <body>
-        <div id="root"><i>Your output would be here...</i></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (err) {
-              let message = "<div style='font-size: 20px;'>Runtime Error</div>"+err;
-              console.error(message);
-              throw err;
-            }
-          })
-        </script>
-        <script>
-          console = {
-            log(message) {
-              document.getElementById('root').innerHTML = message;
-            },
-            info(message) {
-              document.getElementById('root').innerHTML = message;
-              document.getElementById('root').style.color = 'orange';
-            },
-            error(message) {
-              document.getElementById('root').innerHTML = message;
-              document.getElementById('root').style.color = 'red';
-            }
-          }
-        </script>
-      </body>
-    </html>
-  `;
-
   const onClick = async () => {
-    iframe.current.srcdoc = htmlContent;
-
     let result = await esbuild.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -70,8 +34,7 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
 
   return (
@@ -85,12 +48,7 @@ const App = () => {
         />
       </div>
       <button onClick={onClick}>Compile</button>
-      <iframe
-        ref={iframe}
-        sandbox="allow-scripts"
-        title="result"
-        srcDoc={htmlContent}
-      ></iframe>
+      <Preview code={code} />
     </div>
   );
 };
