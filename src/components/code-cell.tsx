c2059aae6a6a9aa7f3ec12ad4e167123042ec088
Abu <abughalib64@gmail.com>
2022-12-14 19:09:33 +0530

    code cell

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import bundler from "../builder/bundler";
+import Resizable from "./resizable";
+
+const CodeCell = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const onClick = async () => {
+    const output = await bundler(input);
+    setCode(output);
+    console.log(code);
+  };
+
+  return (
+    <Resizable direction="vertical">
+      <div className="code-editor">
+        <CodeEditor
+          onChange={(value) => {
+            setInput(value || "");
+          }}
+          initialValue="// Type your code here..."
+        />
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundler from "./builder/bundler";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
-
-  const onClick = async () => {
-    const output = await bundler(input);
-    setCode(output);
-    console.log(code);
-  };
-
   return (
     <div>
-      <div>
-        <CodeEditor
-          onChange={(value) => {
-            setInput(value || "");
-          }}
-          initialValue="// Type your code here..."
-        />
-      </div>
-      <button onClick={onClick}>Compile</button>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };
