53460088ab04e11661e5c38892b89aff9ddbf631
Abu <abughalib64@gmail.com>
2022-12-19 01:44:56 +0530

    deprication removed

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,9 @@
-import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import reducers from "./reducers/reducers";
+import { ActionType } from "./action-types/action_types";
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = configureStore({
+  reducer: reducers,
+  middleware: [thunk],
+});
