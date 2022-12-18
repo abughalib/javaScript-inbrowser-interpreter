fd662ec78ce7f00bfedee5e5d2453bb97698c88f
Abu <abughalib64@gmail.com>
2022-12-19 01:46:37 +0530

    unused import

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import reducers from "./reducers/reducers";
-import { ActionType } from "./action-types/action_types";
 
 export const store = configureStore({
   reducer: reducers,
