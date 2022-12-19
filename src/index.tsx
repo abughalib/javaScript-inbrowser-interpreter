4ac6120b6b6da5310c618205cd1219b1ccceae4c
Abu <abughalib64@gmail.com>
2022-12-19 23:22:29 +0530

    add cellList

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bulmaswatch/cyborg/bulmaswatch.min.css";
-import TextEditor from "./components/text-editor";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import CellList from "./components/cell-list";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
