7ebaf73e99f7eb4d2a6c8f09b2fe1eec112e4840
Abu <abughalib64@gmail.com>
2022-12-20 02:26:05 +0530

    font-awesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bulmaswatch/cyborg/bulmaswatch.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
 import CellList from "./components/cell-list";
