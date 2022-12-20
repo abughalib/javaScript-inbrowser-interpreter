46d7103030a63cb2aeae9164e7969226b967cad7
Abu <abughalib64@gmail.com>
2022-12-21 00:27:25 +0530

    bundler reducer

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,26 @@
-export default {}
+import produce from "immer";
+import { ActionType } from "../action-types/action_types";
+import { Action } from "../actions/actions";
+
+interface BundlesState {
+  [key: string]: {
+    code: string;
+    err: string;
+  } | undefined;
+}
+
+const initialState: BundlesState = {};
+
+const reducer = produce(
+  (state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+      case ActionType.BUNDLE_CREATED:
+        state[action.payload.cellId] = action.payload.bundle;
+        return state;
+      default:
+        return state;
+    }
+  }
+);
+
+export default reducer;
